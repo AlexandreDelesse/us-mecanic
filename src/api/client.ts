060f8df49b0e5e4bc15f93b31f392d3753f766a9
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2025-06-24 18:11:23 +0200

    change geoapi url

--- src/api/client.ts
+++ src/api/client.ts
@@ -4,7 +4,7 @@ import keycloak from "../Keycloak/Keycloak";
 // const PORT = import.meta.env.VITE_API_PORT || "8090";
 const BASE_URL = `https://notification-api.delesse.net`;
 // const BASE_URL = `https://intranet.urgencesante.fr:${PORT}`;
-const GEO_BASE_URL = `https://intranet.urgencesante.fr:8091/api/`;
+const GEO_BASE_URL = `https://intranet.urgencesante.fr:8091/`;
 
 const client = axios.create({
   baseURL: BASE_URL,
