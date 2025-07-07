239c5bb0ffc1e3a082f470bac9c108694c64b0a7
AlexandreDelesse <alexandre.delesse@isen.yncrea.fr>
2025-07-07 16:14:43 +0200

    hotfix: model trip

--- src/components/Geoloc/Geoloc.api.ts
+++ src/components/Geoloc/Geoloc.api.ts
@@ -2,9 +2,20 @@ import { geoClient } from "../../api/client";
 import type { Trip } from "./Geoloc.model";
 
 export const getGeoloc = async (immat: string, tripId: string) => {
-  return (
+  const geolocData = (
     await geoClient.get(`CertifyTrip?gTripId=${tripId}&strImmat=${immat}`)
   ).data;
+
+  const tripData: Trip = {
+    Arrival: geolocData.Arrival,
+    Departure: geolocData.Departure,
+    DrivePoints: geolocData.DrivePointsModels,
+    StopPoints: geolocData.StopPointsModel,
+    TripDescription: geolocData.TripDescription,
+    TripId: geolocData.TripId,
+  };
+
+  return tripData;
 };
 
 export const postGeoloc = async (trip: Trip) => {
